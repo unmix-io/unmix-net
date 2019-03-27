d8229b2f099f90b6551e1b88d3a33e418c1ec2ae
kaufmann-a <kaufmann-a@hotmail.ch>
2019-03-27 12:56:24 +0100

    Added parameter for pretrained weights, added new package isolator

---
+++ unmix/source/isolator/vocalisolator.py
@@ -0,0 +1,12 @@
+"""
+Seperates vocals from a given song
+"""
+
+__author__ = 'David Flury, Andreas Kaufmann, Raphael Müller'
+__email__ = "info@unmix.io"
+
+
+class VocalIsolator:
+
+    def __init__(self):
+        None
--- unmix/source/training.py
+++ unmix/source/training.py
@@ -23,21 +23,39 @@ if __name__ == "__main__":
                         type=str, help="Environment and training configuration.")
     parser.add_argument('--workingdir', default=os.getcwd(), 
                         type=str, help="Working directory (default: current directory)")
+    parser.add_argument('--working_mode', default='train', type=str,
+                        help="Enter 'train' or 'separate', if entered 'separate', songfile in --songfile parameter must be provided")
+    parser.add_argument('--songfiles', default='', type=str, help="Audiofiles to be splited")
 
     args = parser.parse_args()
     console.info("Arguments: ", str(args))
 
-    start = time.time()
+    if args.working_mode == 'train':
+        start = time.time()
 
-    config = Configuration.initialize(args.configuration, args.workingdir)
+        config = Configuration.initialize(args.configuration, args.workingdir)
 
-    console.h1("unmix.io Neuronal Network Training Application")
-    console.info("Environment: %s" % Configuration.get('environment.name'))
-    console.info("Collection: %s" % Configuration.get('collection.folder'))
-    console.info("Model: %s" % Configuration.get('training.model.name'))
-    
-    unmixnet = UnmixNet()
-    unmixnet.train()
-    end = time.time()
+		console.h1("unmix.io Neuronal Network Training Application")
+		console.info("Environment: %s" % Configuration.get('environment.name'))
+		console.info("Collection: %s" % Configuration.get('collection.folder'))
+		console.info("Model: %s" % Configuration.get('training.model.name'))
+		
+		unmixnet = UnmixNet()
+		unmixnet.train()
+        # Todo: unmixnet.save_weights()
 
-    console.info("Finished processing in %d [ms]." % (end - start))
+        end = time.time()
+        console.info("Finished processing in %d [ms]." % (end - start))
+    elif args.working_mode == 'separate':
+        start = time.time()
+
+        config = Configuration.initialize(args.configuration, args.workingdir)
+
+        console.h1("unmix.io Neuronal Network Training Application")
+        unmixnet = UnmixNet()
+        unmixnet.load_weights(Configuration.get_path(os.path.join(Configuration.get_path('environment.weights.folder'), config.file_name)))
+        # ToDo Implement song processing
+        end = time.time()
+    else:
+        console.info("For parameter --workingmode either 'train' or 'separate' must be entered")
+
