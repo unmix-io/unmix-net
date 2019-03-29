49c49d985681ba6ee18f114ce4890969c4cb782c
David Flury <david@flury.email>
2019-03-29 13:42:34 +0100

    Work on callbacks

--- unmix/source/callbacks/callbacksfactory.py
+++ unmix/source/callbacks/callbacksfactory.py
@@ -9,7 +9,7 @@ __email__ = "info@unmix.io"
 
 import os
 
-from keras.callbacks import ModelCheckpoint, TensorBoard, EarlyStopping
+from keras.callbacks import ModelCheckpoint, TensorBoard, EarlyStopping, ReduceLROnPlateau
 
 from unmix.source.callbacks.errorvisualization import ErrorVisualization
 from unmix.source.configuration import Configuration
@@ -22,27 +22,36 @@ class CallbacksFactory(object):
     def build():
         configs = Configuration.get('training.callbacks', False)
         callbacks = []
-        if hasattr(configs, 'weights'):
-            callbacks.append(CallbacksFactory.weights(configs.weights))
+        if hasattr(configs, 'model_checkpoint'):
+            callbacks.append(CallbacksFactory.model_checkpoint(configs.model_checkpoint))
         if hasattr(configs, 'early_stopping'):
             callbacks.append(CallbacksFactory.early_stopping(configs.early_stopping))
         if hasattr(configs, 'tensorboard'):
             callbacks.append(CallbacksFactory.tensorboard(configs.tensorboard))
+        if hasattr(configs, 'reduce_learningrate'):
+            callbacks.append(CallbacksFactory.reduce_learningrate(configs.reduce_learningrate))
         return callbacks
 
     @staticmethod
-    def weights(config):
+    def model_checkpoint(config):
         folder = Configuration.get_path("environment.weights.folder")
-        file = os.path.join(folder, config.file_name)
-        return ModelCheckpoint(filepath=file,
-                               verbose=config.verbose,
-                               save_best_only=config.best_only)
+        path = os.path.join(folder, config.file_name)
+        return ModelCheckpoint(filepath=path, monitor=config.monitor,
+                               save_best_only=config.best_only,
+                               save_weights_only=config.weights_only,
+                               mode=config.mode, period=config.period, verbose=config.verbose)
 
     @staticmethod
     def early_stopping(config):
-        return EarlyStopping(min_delta=config.min_delta,
+        return EarlyStopping(monitor=config.monitor,
+                             min_delta=config.min_delta,
                              patience=config.patience,
                              verbose=config.verbose)
+    
+    @staticmethod
+    def reduce_learningrate(config):
+        return ReduceLROnPlateau(monitor=config.monitor, factor=config.factor,
+                                 patience=config.patience, min_lr=config.min_learningrate)
 
     @staticmethod
     def error_visualization(bot):
@@ -50,10 +59,12 @@ class CallbacksFactory(object):
 
     @staticmethod
     def tensorboard(config):
-        path = Configuration.get_path("environment.tensorboard_folder")
-        log_dir = os.path.join(path, converter.get_timestamp())
-        return TensorBoard(log_dir=log_dir,
-                           write_images=config.write_images,
-                           write_grads=config.write_grads,
+        path = Configuration.get_path("training.callbacks.tensorboard.folder")
+        return TensorBoard(log_dir=path,
                            histogram_freq=config.histogram_freq,
+                           write_graph=config.write_graph,
+                           write_grads=config.write_grads,
+                           write_images=config.write_images,
+                           embeddings_freq=config.embeddings_freq,
+                           update_freq=config.update_freq,
                            batch_size=Configuration.get("training.batch_size"))
