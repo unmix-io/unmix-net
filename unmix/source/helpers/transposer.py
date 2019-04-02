1b2ac189ee7c61f9160319ea34d705907b8a2fb2
David Flury <david@flury.email>
2019-04-02 23:06:44 +0200

    Fix chop transposing

--- unmix/source/helpers/transposer.py
+++ unmix/source/helpers/transposer.py
@@ -9,15 +9,12 @@ __author__ = 'David Flury, Andreas Kaufmann, Raphael Müller'
 __email__ = "info@unmix.io"
 
 
-PRE_TRANSPOSE_DIMENSIONS = [[0], [1, 0], [1, 0, 2], [2, 0, 1, 3]]
-POST_TRANSPOSE_DIMENSIONS = [[0], [0, 1], [0, 2, 1], [0, 2, 1, 3], [0, 2, 1, 3, 4]]
+TRANSPOSE_DIMENSIONS = [[0], [1, 0], [1, 0, 2], [2, 0, 1, 3]]
 
 
-def pre_transpose(input):
-   return input.transpose(*PRE_TRANSPOSE_DIMENSIONS[len(input.shape) - 1])
+def transpose_step(input):
+   return input.transpose(*TRANSPOSE_DIMENSIONS[len(input.shape) - 1])
 
-def post_transpose(input):
-   return input.transpose(*POST_TRANSPOSE_DIMENSIONS[len(input.shape) - 1])
 
 def pre_post_transpose(input, func):
-   return post_transpose(func(pre_transpose(input)))
+   return transpose_step(func(transpose_step(input)))
--- unmix/test/chopper/testpadchop.py
+++ unmix/test/chopper/testpadchop.py
@@ -19,44 +19,44 @@ def test_pad_none():
     input = np.array([range(1,10)])
     chopper = Chopper(10)
     chop = transposer.pre_post_transpose(input, lambda x: chopper.pad_chop(x, 0, 2))
-    assert len(chop) == 2
-    assert chop[0] != 0
-    assert chop[-1] != 0
+    assert len(chop[0]) == 2
+    assert chop[0][0] != 0
+    assert chop[0][-1] != 0
 
 
 def test_pad_left():
     input = np.array([range(1,10)])
     chopper = Chopper(10)
     chop = transposer.pre_post_transpose(input, lambda x: chopper.pad_chop(x, -2, 2))
-    assert len(chop) == 4
-    assert chop[0] == 0
-    assert chop[1] == 0
-    assert chop[2] != 0
-    assert chop[3] != 0
+    assert len(chop[0]) == 4
+    assert chop[0][0] == 0
+    assert chop[0][1] == 0
+    assert chop[0][2] != 0
+    assert chop[0][3] != 0
 
 
 def test_pad_right():
     input = np.array([range(1,10)])
     chopper = Chopper(10)
     chop = transposer.pre_post_transpose(input, lambda x: chopper.pad_chop(x, 7, 11))
-    assert len(chop) == 4
-    assert chop[0] != 0
-    assert chop[1] != 0
-    assert chop[2] == 0
-    assert chop[3] == 0
+    assert len(chop[0]) == 4
+    assert chop[0][0] != 0
+    assert chop[0][1] != 0
+    assert chop[0][2] == 0
+    assert chop[0][3] == 0
 
 
 def test_pad_left_right():
     input = np.array([range(1,3)])
     chopper = Chopper(10)
     chop = transposer.pre_post_transpose(input, lambda x: chopper.pad_chop(x, -2, 4))
-    assert len(chop) == 6
-    assert chop[0] == 0
-    assert chop[1] == 0
-    assert chop[2] != 0
-    assert chop[3] != 0
-    assert chop[4] == 0
-    assert chop[5] == 0
+    assert len(chop[0]) == 6
+    assert chop[0][0] == 0
+    assert chop[0][1] == 0
+    assert chop[0][2] != 0
+    assert chop[0][3] != 0
+    assert chop[0][4] == 0
+    assert chop[0][5] == 0
 
 
 if __name__ == "__main__":
