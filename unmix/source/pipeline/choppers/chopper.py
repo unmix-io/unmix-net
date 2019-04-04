2246edc2f614eee17e11654b1a6fab519c1fd948
Raphael Müller <raaphii@gmail.com>
2019-04-04 11:11:36 +0200

    Performance and memory optimizations: chopper

--- unmix/source/pipeline/choppers/chopper.py
+++ unmix/source/pipeline/choppers/chopper.py
@@ -10,6 +10,7 @@ __email__ = "info@unmix.io"
 
 
 import numpy as np
+import math
 
 from unmix.source.exceptions.configurationerror import ConfigurationError
 import unmix.source.helpers.transposer as transposer
@@ -20,22 +21,31 @@ class Chopper:
     def __init__(self, step):
         self.step = step
 
-    def get_chop(self, input, index, size):
+    # def get_chop(self, input, index, size):
+    #     start = self.step * index - int(size / 2)
+    #     end = start + size
+    #     chop = transposer.pre_post_transpose(input, lambda x: self.pad_chop(x, start, end))
+    #     return chop
+
+    # def pad_chop(self, input, start, end):
+    #     prepend = np.zeros((0, *input.shape[1:]))
+    #     if start < 0:
+    #         prepend = np.zeros((-start, *input.shape[1:]))
+    #         end += -start
+    #         start = 0
+    #     append = np.zeros((0, *input.shape[1:]))
+    #     if end >= len(input):
+    #         append = np.zeros((end - len(input), *input.shape[1:]))
+    #     return np.concatenate((prepend, input, append), axis=0)[start:end]
+
+    def chop_n_pad(self, input, index, size):
         start = self.step * index - int(size / 2)
         end = start + size
-        chop = transposer.pre_post_transpose(input, lambda x: self.pad_chop(x, start, end))
-        return chop
+        pad_count_left = abs(max(0, -start))
+        pad_count_right = abs(max(0, end - input.shape[1]))
+        chunk = input[:, max(0, start):min(end, input.shape[1])]
+        return np.pad(chunk, ((0,0),(pad_count_left,pad_count_right)), "constant")
 
-    def pad_chop(self, input, start, end):
-        prepend = np.zeros((0, *input.shape[1:]))
-        if start < 0:
-            prepend = np.zeros((-start, *input.shape[1:]))
-            end += -start
-            start = 0
-        append = np.zeros((0, *input.shape[1:]))
-        if end >= len(input):
-            append = np.zeros((end - len(input), *input.shape[1:]))
-        return np.concatenate((prepend, input, append), axis=0)[start:end]
-
-    def calculate_chops(self, width):
-        return int(width / self.step)
+    def calculate_chops(self, input_width, size):
+        max_width = input_width + int(size / 2) # at each end of the input, we maximal pad size / 2
+        return int(math.ceil(max_width / self.step))
--- unmix/test/chopper/testchopper.py
+++ unmix/test/chopper/testchopper.py
@@ -11,76 +11,140 @@ __email__ = "info@unmix.io"
 
 import numpy as np
 
-from unmix.source.choppers.chopper import Chopper
+from unmix.source.pipeline.choppers.chopper import Chopper
+
+def test_start():
+    input = np.ones((1000,64))
+    chopper = Chopper(64)
+    chop = chopper.chop_n_pad(input, 0, 64)
+    # When we access the first item, the chopper adds padding of -size - should be zero
+    assert chop.shape[1] == 64
+    assert chop.shape[0] == 1000
+    assert np.all(chop[:, 0:32] == 0)
+    assert np.all(chop[:, 32:] == 1)
 
 
-def test_horizontal_overlap_2d_2():
-    input = np.array([range(9)])
-    chopper = Chopper(Chopper.DIRECTION_HORIZONTAL, Chopper.MODE_OVERLAP, 2)
-    chops = chopper.chop(input)
-    assert len(chops) == 8
-    input = np.array([range(10)])
-    chopper = Chopper(Chopper.DIRECTION_HORIZONTAL, Chopper.MODE_OVERLAP, 2)
-    chops = chopper.chop(input)
-    assert len(chops) == 9
-    input = np.array([range(11)])
-    chopper = Chopper(Chopper.DIRECTION_HORIZONTAL, Chopper.MODE_OVERLAP, 2)
-    chops = chopper.chop(input)
-    assert len(chops) == 10
+def test_end():
+    input = np.ones((1000,10))
+    chopper = Chopper(10)
+    chop = chopper.chop_n_pad(input, 1, 10)
+    assert chop.shape[1] == 10
+    assert chop.shape[0] == 1000
+    assert np.all(chop[:, 0:5] == 1)
+    assert np.all(chop[:, 5:] == 0)
+
+def test_end_odd():
+    input = np.ones((1000,9))
+    chopper = Chopper(7)
+    chop = chopper.chop_n_pad(input, 1, 5)
+    assert chop.shape[1] == 5
+    assert chop.shape[0] == 1000
+    assert np.all(chop[:, 0:4] == 1)
+    assert np.all(chop[:, 4:] == 0)
+
+def test_middle():
+    input = np.ones((1000,2000))
+    chopper = Chopper(10)
+    chop = chopper.chop_n_pad(input, 35, 10)
+    assert chop.shape[1] == 10
+    assert chop.shape[0] == 1000
+    assert np.all(chop[:, :] == 1)
 
 
-def test_horizontal_overlap_2d_4():
-    input = np.array([range(11)])
-    chopper = Chopper(Chopper.DIRECTION_HORIZONTAL, Chopper.MODE_OVERLAP, 4)
-    chops = chopper.chop(input)
-    assert len(chops) == 4
-    input = np.array([range(12)])
-    chopper = Chopper(Chopper.DIRECTION_HORIZONTAL, Chopper.MODE_OVERLAP, 4)
-    chops = chopper.chop(input)
-    assert len(chops) == 5
-    input = np.array([range(13)])
-    chopper = Chopper(Chopper.DIRECTION_HORIZONTAL, Chopper.MODE_OVERLAP, 4)
-    chops = chopper.chop(input)
-    assert len(chops) == 5
+def test_lengths():
+    chopper = Chopper(2)
+    assert chopper.calculate_chops(4, 2) == 3
+    assert chopper.calculate_chops(5, 2) == 3
+    assert chopper.calculate_chops(6, 2) == 4
+    assert chopper.calculate_chops(7, 2) == 4
+    assert chopper.calculate_chops(8, 2) == 5
+
+def test_chopping():
+    size = 10
+    input = np.ones((1000,1998))
+    chopper = Chopper(size)
+    chop_count = chopper.calculate_chops(input.shape[1], size)
+
+    for i in range(chop_count):
+        chop = chopper.chop_n_pad(input, i, size)
+
+        if i == 0:
+            assert np.all(chop[:, 0:int(size/2)] == 0)
+            assert np.all(chop[:, int(size/2):] == 1)
+        elif i == chop_count - 1:
+            remainder = (input.shape[1] - int(size/2)) % size
+            assert np.all(chop[:, 0:remainder] == 1)
+            assert np.all(chop[:, remainder:] == 0)
+        else:
+            assert np.all(chop[:, :] == 1)
+
+# def test_horizontal_overlap_2d_2():
+#     input = np.array([range(9)])
+#     chopper = Chopper(Chopper.DIRECTION_HORIZONTAL, Chopper.MODE_OVERLAP, 2)
+#     chops = chopper.chop(input)
+#     assert len(chops) == 8
+#     input = np.array([range(10)])
+#     chopper = Chopper(Chopper.DIRECTION_HORIZONTAL, Chopper.MODE_OVERLAP, 2)
+#     chops = chopper.chop(input)
+#     assert len(chops) == 9
+#     input = np.array([range(11)])
+#     chopper = Chopper(Chopper.DIRECTION_HORIZONTAL, Chopper.MODE_OVERLAP, 2)
+#     chops = chopper.chop(input)
+#     assert len(chops) == 10
 
 
-def test_horizontal_stepwise_2d_2():
-    input = np.array([range(9)])
-    chopper = Chopper(Chopper.DIRECTION_HORIZONTAL, Chopper.MODE_STEPWISE, 3)
-    chops = chopper.chop(input)
-    assert len(chops) == 7
-    input = np.array([range(10)])
-    chopper = Chopper(Chopper.DIRECTION_HORIZONTAL, Chopper.MODE_STEPWISE, 3)
-    chops = chopper.chop(input)
-    assert len(chops) == 8
+# def test_horizontal_overlap_2d_4():
+#     input = np.array([range(11)])
+#     chopper = Chopper(Chopper.DIRECTION_HORIZONTAL, Chopper.MODE_OVERLAP, 4)
+#     chops = chopper.chop(input)
+#     assert len(chops) == 4
+#     input = np.array([range(12)])
+#     chopper = Chopper(Chopper.DIRECTION_HORIZONTAL, Chopper.MODE_OVERLAP, 4)
+#     chops = chopper.chop(input)
+#     assert len(chops) == 5
+#     input = np.array([range(13)])
+#     chopper = Chopper(Chopper.DIRECTION_HORIZONTAL, Chopper.MODE_OVERLAP, 4)
+#     chops = chopper.chop(input)
+#     assert len(chops) == 5
 
 
-def test_horizontal_split_3d_64():
-    input = np.empty((769, 2000, 2))
-    chopper = Chopper(Chopper.DIRECTION_HORIZONTAL, Chopper.MODE_SPLIT, 64)
-    chops = chopper.chop(input)
-    assert len(chops) == 31  # floor(2000/64)
+# def test_horizontal_stepwise_2d_2():
+#     input = np.array([range(9)])
+#     chopper = Chopper(Chopper.DIRECTION_HORIZONTAL, Chopper.MODE_STEPWISE, 3)
+#     chops = chopper.chop(input)
+#     assert len(chops) == 7
+#     input = np.array([range(10)])
+#     chopper = Chopper(Chopper.DIRECTION_HORIZONTAL, Chopper.MODE_STEPWISE, 3)
+#     chops = chopper.chop(input)
+#     assert len(chops) == 8
 
 
-def test_horizontal_overlap_3d_64():
-    input = np.empty((769, 2000, 2))
-    chopper = Chopper(Chopper.DIRECTION_HORIZONTAL, Chopper.MODE_OVERLAP, 64)
-    chops = chopper.chop(input)
-    assert len(chops) == 61  # floor((2000-32)/(32))
+# def test_horizontal_split_3d_64():
+#     input = np.empty((769, 2000, 2))
+#     chopper = Chopper(Chopper.DIRECTION_HORIZONTAL, Chopper.MODE_SPLIT, 64)
+#     chops = chopper.chop(input)
+#     assert len(chops) == 31  # floor(2000/64)
 
 
-def test_horizontal_stepwise_3d_64():
-    input = np.empty((769, 2000, 2))
-    chopper = Chopper(Chopper.DIRECTION_HORIZONTAL, Chopper.MODE_STEPWISE, 64)
-    chops = chopper.chop(input)
-    assert len(chops) == 1937  # 2000 - 64 + 1
+# def test_horizontal_overlap_3d_64():
+#     input = np.empty((769, 2000, 2))
+#     chopper = Chopper(Chopper.DIRECTION_HORIZONTAL, Chopper.MODE_OVERLAP, 64)
+#     chops = chopper.chop(input)
+#     assert len(chops) == 61  # floor((2000-32)/(32))
+
+
+# def test_horizontal_stepwise_3d_64():
+#     input = np.empty((769, 2000, 2))
+#     chopper = Chopper(Chopper.DIRECTION_HORIZONTAL, Chopper.MODE_STEPWISE, 64)
+#     chops = chopper.chop(input)
+#     assert len(chops) == 1937  # 2000 - 64 + 1
 
 
 if __name__ == "__main__":
-    test_horizontal_overlap_2d_2()
-    test_horizontal_overlap_2d_4()
-    test_horizontal_stepwise_2d_2()
-    test_horizontal_split_3d_64()
-    test_horizontal_overlap_3d_64()
-    test_horizontal_stepwise_3d_64()
+    test_start()
+    test_end()
+    test_end_odd()
+    test_middle()
+    test_lengths()
+    test_chopping()
     print("Test run successful.")
