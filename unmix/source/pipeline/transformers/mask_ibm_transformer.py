8847bd6f470330ebd4820e8c8a1c8b73de3b757e
Raphael Müller <raaphii@gmail.com>
2019-04-08 23:38:39 +0200

    Added prototype default-mask-ibm transformer

---
+++ unmix/source/pipeline/transformers/mask_ibm_transformer.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# coding: utf8
+
+"""
+Transform input to normalized amplitude spectrograms, target to a probability mask for vocals
+"""
+
+__author__ = 'David Flury, Andreas Kaufmann, Raphael Müller'
+__email__ = "info@unmix.io"
+
+
+import numpy as np
+
+from unmix.source.exceptions.configurationerror import ConfigurationError
+from unmix.source.helpers import audiohandler
+from unmix.source.helpers import reducer
+from unmix.source.pipeline.choppers.chopper import Chopper
+import unmix.source.pipeline.normalizers.normalizer_real_imag as normalizer
+from unmix.source.helpers.masker import mask
+
+class IBMMaskTransformer:
+
+    NAME = "mask_ibm"
+
+    def __init__(self, size, step, shuffle):
+        self.size = size
+        self.step = step
+        self.shuffle = shuffle
+        self.chopper = Chopper(step)
+
+    def run(self, name, mix, vocals, index):
+        'Returns shapes (769,size,1), (769,step,1)'
+        input = self.chopper.chop_n_pad(mix[0], index, self.size)
+
+        # Calculate mask
+        mix_slice = self.chopper.chop_n_pad(mix[0], index, self.step)
+        vocal_slice = self.chopper.chop_n_pad(vocals[0], index, self.step)
+
+        instrumental_slice = mix_slice - vocal_slice
+        
+        instrumental_magnitude = np.abs(instrumental_slice)
+        vocal_magnitude = np.abs(vocal_slice)
+
+        target_mask = np.empty_like(instrumental_magnitude)
+        target_mask[instrumental_magnitude <= vocal_magnitude] = 1
+        target_mask[instrumental_magnitude > vocal_magnitude] = 0
+        target_mask = np.reshape(target_mask, target_mask.shape + (1,))
+
+        return normalizer.normalize(input)[0], target_mask
+
+    def calculate_items(self, width):
+        return self.chopper.calculate_chops(width, self.size)
+
+    def prepare_input(self, mix, index):
+        'Selects one training slice and performs preparation steps for the input (mix).'
+        input = self.chopper.chop_n_pad(mix, index, self.size)
+        return normalizer.normalize(input)
+
+    def untransform_target(self, mix, predicted_mask, index, transform_info):
+        'Transforms predicted slices back to a format which corresponds to the training data (ready to process back to audio).'
+        predicted_mask = np.reshape(predicted_mask, predicted_mask.shape[0:2])
+        mix_slice = self.chopper.chop_n_pad(mix, index, self.step)
+        mix_magnitude = np.abs(mix_slice)
+        predicted_mask = np.clip(predicted_mask, 0, 1)
+        vocal_magnitude = mix_magnitude * predicted_mask
+        vocals = vocal_magnitude * np.exp( np.angle(mix_slice) * 1j )
+
+        return vocals
--- unmix/source/pipeline/transformers/transformerfactory.py
+++ unmix/source/pipeline/transformers/transformerfactory.py
@@ -14,6 +14,7 @@ import keras.backend as keras
 from unmix.source.configuration import Configuration
 from unmix.source.exceptions.configurationerror import ConfigurationError
 from unmix.source.pipeline.transformers.masktransformer import MaskTransformer
+from unmix.source.pipeline.transformers.mask_ibm_transformer import IBMMaskTransformer
 from unmix.source.pipeline.transformers.windowtransformer import WindowTransformer
 
 
@@ -28,6 +29,8 @@ class TransformerFactory(object):
                 return MaskTransformer(options.size, options.step, options.shuffle)
             if name == WindowTransformer.NAME:
                 return WindowTransformer(options.size, options.step, options.shuffle, options.save_audio)
+            if name == IBMMaskTransformer.NAME:
+                return IBMMaskTransformer(options.size, options.step, options.shuffle)
         except:
             raise ConfigurationError('transformation.options')
         raise ConfigurationError('transformation.name')
