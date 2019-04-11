0bfecef7f78fdef4b32ebf3d2ff3d7de9c3442be
David Flury <david@flury.email>
2019-04-11 22:47:53 +0200

    Fix predict file naming for runs folder and other

--- predict.py
+++ predict.py
@@ -26,6 +26,21 @@ from unmix.source.logging.logger import Logger
 
 AUDIO_FILE_EXTENSIONS = ['.wav', '.mp3']
 
+
+def save_prediction(type, prediction, sample_rate, file, folder=''):
+    track = np.array(librosa.istft(prediction))
+    name = os.path.splitext(os.path.basename(file))[0]
+    file_name = converter.get_timestamp() + "_" + name + '_predicted_%s.wav' % type
+    if folder:
+        output_file = os.path.join(
+            folder, file_name)
+    else:
+        output_file = os.path.join(os.path.dirname(file), file_name)
+
+    librosa.output.write_wav(output_file, track, sample_rate, norm=False)
+    Logger.info("Output prediction file: %s" % output_file)
+
+
 if __name__ == "__main__":
     global config
 
@@ -36,11 +51,11 @@ if __name__ == "__main__":
                         type=str, help="Environment and training configuration.")
     parser.add_argument('--weights', default='',
                         type=str, help="Pretrained weights file (overwrites configuration).")
-    parser.add_argument('--workingdir', default=os.getcwd(), 
+    parser.add_argument('--workingdir', default=os.getcwd(),
                         type=str, help="Working directory (default: current directory).")
     parser.add_argument('--sample_rate', default=11025,
                         type=str, help="Target sample rate which the model can process.")
-    parser.add_argument('--fft_window', default=1536, 
+    parser.add_argument('--fft_window', default=1536,
                         type=str, help="FFT window size the model was trained on.")
     parser.add_argument('--song', default='',
                         type=str, help="Input audio file to split vocals and instrumental.")
@@ -51,24 +66,27 @@ if __name__ == "__main__":
     start = time.time()
 
     if args.run_folder:
-        args.workingdir = filehelper.build_abspath(args.run_folder, os.getcwd())
-        args.configuration = os.path.join(args.workingdir, 'configuration.jsonc')
-        args.weights = filehelper.get_latest(os.path.join(args.workingdir, 'weights'), '*weights*.h5')
-          
+        args.workingdir = filehelper.build_abspath(
+            args.run_folder, os.getcwd())
+        args.configuration = os.path.join(
+            args.workingdir, 'configuration.jsonc')
+        args.weights = filehelper.get_latest(os.path.join(
+            args.workingdir, 'weights'), '*weights*.h5')
+
     Configuration.initialize(args.configuration, args.workingdir, False)
     Logger.initialize(False)
- 
+
     if args.run_folder:
-        prediction_folder = Configuration.build_path('predictions')        
+        prediction_folder = Configuration.build_path('predictions')
     else:
         prediction_folder = ''
 
     if os.path.isdir(args.song):
         args.songs = args.song
         args.song = ''
-    
+
     Logger.info("Arguments: ", str(args))
-  
+
     song_files = []
     if args.song:
         song_files = [args.song]
@@ -79,36 +97,28 @@ if __name__ == "__main__":
                 song_files.append(file)
 
     Logger.info("Found %d songs to predict." % len(song_files))
-        
+
     engine = Engine()
     engine.load_weights(args.weights)
 
     for song_file in song_files:
         try:
             # Load song and create spectrogram with librosa
-            audio, sample_rate = librosa.load(song_file, mono=True, sr=args.sample_rate)
+            audio, sample_rate = librosa.load(
+                song_file, mono=True, sr=args.sample_rate)
             mix = librosa.stft(audio, args.fft_window)
-            
+
             Logger.info("Start predicting song: %s." % song_file)
             predicted_vocals, predicted_instrumental = engine.predict(mix)
 
             # Convert back to wav audio file
-            vocals = np.array(librosa.istft(predicted_vocals))
-            instrumental = np.array(librosa.istft(predicted_instrumental))
-
-            if prediction_folder:
-                output_file = os.path.join(prediction_folder, converter.get_timestamp() + "_" + os.path.splitext(os.path.basename(song_file))[0])
-            else:
-                output_file = os.path.splitext(song_file)[0]
-
-            librosa.output.write_wav(output_file + "_vocals.wav", vocals, sample_rate, norm=False)
-            Logger.info("Output prediction file: %s_vocals.wav." % output_file)
-
-            librosa.output.write_wav(output_file + "_instrumental.wav", instrumental, sample_rate, norm=False)
-            Logger.info("Output prediction file: %s_instrumentals.wav." % output_file)
+            save_prediction("vocals", predicted_vocals,
+                            args.sample_rate, song_file, prediction_folder)
+            save_prediction("instrumental", predicted_instrumental,
+                            args.sample_rate, song_file, prediction_folder)
         except Exception as e:
-            Logger.error("Error while predicting song '%s': %s." % (song_file, str(e)))
+            Logger.error(
+                "Error while predicting song '%s': %s." % (song_file, str(e)))
 
     end = time.time()
-
     Logger.info("Finished processing in %d [s]." % (end - start))
