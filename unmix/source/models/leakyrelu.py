8dc29f56d61138aad058c3f48b1e7a8d7c714bc4
David Flury <david@flury.email>
2019-04-14 23:23:03 +0200

    Nothing to see here, move along.

--- unmix/source/models/leakyrelu.py
+++ unmix/source/models/leakyrelu.py
@@ -24,41 +24,41 @@ class LeakyReluModel(BaseModel):
     def build(self, config):
         alpha1 = config.alpha1
         alpha2 = config.alpha2
-        rate = config.rate
+        dropout_rate = config.dropout_rate
         channels = 1
         batch_size = Configuration.get("training.batch_size")
 
         mashup = Input(batch_shape=(batch_size, 769, 64, 1), name='input')
         padding = ZeroPadding2D(((3, 0), (0, 0)))(mashup)
-        dropout = Dropout(rate=rate)(padding)
+        dropout = Dropout(rate=dropout_rate)(padding)
 
         conv_a = Conv2D(filters=64, kernel_size=3, padding='same')(dropout)
         conv_a = LeakyReLU(alpha=alpha1)(conv_a)
-        conv_a = Dropout(rate=rate)(conv_a)
+        conv_a = Dropout(rate=dropout_rate)(conv_a)
 
         conv = Conv2D(filters=64, kernel_size=4, strides=2, padding='same', use_bias=False)(conv_a)
         conv = LeakyReLU(alpha=alpha1)(conv)
-        conv = Dropout(rate=rate)(conv)
+        conv = Dropout(rate=dropout_rate)(conv)
 
         conv = BatchNormalization()(conv)
 
         conv_b = Conv2D(filters=64, kernel_size=3, padding='same')(conv)
         conv_b = LeakyReLU(alpha=alpha1)(conv_b)
-        conv_b = Dropout(rate=rate)(conv_b)
+        conv_b = Dropout(rate=dropout_rate)(conv_b)
 
         conv = Conv2D(filters=64, kernel_size=4, strides=2, padding='same', use_bias=False)(conv_b)
         conv = LeakyReLU(alpha=alpha1)(conv)
-        conv = Dropout(rate=rate)(conv)
+        conv = Dropout(rate=dropout_rate)(conv)
 
         conv = BatchNormalization()(conv)
 
         conv = Conv2D(filters=128, kernel_size=3, padding='same')(conv)
         conv = LeakyReLU(alpha=alpha1)(conv)
-        conv = Dropout(rate=rate)(conv)
+        conv = Dropout(rate=dropout_rate)(conv)
 
         conv = Conv2D(filters=128, kernel_size=3, padding='same', use_bias=False)(conv)
         conv = LeakyReLU(alpha=alpha1)(conv)
-        conv = Dropout(rate=rate)(conv)
+        conv = Dropout(rate=dropout_rate)(conv)
 
         conv = BatchNormalization()(conv)
         conv = UpSampling2D(size=(2, 2))(conv)
@@ -67,11 +67,11 @@ class LeakyReluModel(BaseModel):
 
         conv = Conv2D(filters=64, kernel_size=3, padding='same')(conv)
         conv = LeakyReLU(alpha=alpha1)(conv)
-        conv = Dropout(rate=rate)(conv)
+        conv = Dropout(rate=dropout_rate)(conv)
 
         conv = Conv2D(filters=64, kernel_size=3, padding='same', use_bias=False)(conv)
         conv = LeakyReLU(alpha=alpha1)(conv)
-        conv = Dropout(rate=rate)(conv)
+        conv = Dropout(rate=dropout_rate)(conv)
 
         conv = BatchNormalization()(conv)
         conv = UpSampling2D(size=(2, 2))(conv)
@@ -80,15 +80,15 @@ class LeakyReluModel(BaseModel):
 
         conv = Conv2D(filters=64, kernel_size=3, padding='same')(conv)
         conv = LeakyReLU(alpha=alpha2)(conv)
-        conv = Dropout(rate=rate)(conv)
+        conv = Dropout(rate=dropout_rate)(conv)
 
         conv = Conv2D(filters=64, kernel_size=3, padding='same')(conv)
         conv = LeakyReLU(alpha=alpha2)(conv)
-        conv = Dropout(rate=rate)(conv)
+        conv = Dropout(rate=dropout_rate)(conv)
 
         conv = Conv2D(filters=32, kernel_size=3, padding='same')(conv)
         conv = LeakyReLU(alpha=alpha2)(conv)
-        conv = Dropout(rate=rate)(conv)
+        conv = Dropout(rate=dropout_rate)(conv)
 
         conv = Conv2D(filters=channels, kernel_size=3, padding='same')(conv)
         conv = Cropping2D(((3, 0), (0, 0)))(conv)
