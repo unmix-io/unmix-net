bc14b843d90a6cb820c45103bf8768664118d63a
Raphael Müller <raaphii@gmail.com>
2019-04-17 09:17:58 +0200

    Changed mask model

--- unmix/source/models/mask.py
+++ unmix/source/models/mask.py
@@ -6,7 +6,7 @@ Keras model for training using a mask based appoach.
 """
 
 
-from keras.layers import Dropout, Conv2D, MaxPooling2D, Flatten, Dense, LeakyReLU, Reshape, Input, ReLU, Activation
+from keras.layers import Dropout, Conv2D, MaxPooling2D, Flatten, Dense, LeakyReLU, Reshape, Input, ReLU, Activation, BatchNormalization
 from keras.models import Sequential, Model
 from keras.activations import sigmoid
 from functools import reduce 
@@ -26,29 +26,33 @@ class MaskModel(BaseModel):
         input_shape = (769, transformation.size, 1)
 
         alpha1 = 0.3
+        dropout = 0.1
+        filter_factor = 2
         
         model = Sequential()
-        model.add(Conv2D(32, (3, 3), padding='same',
-                         input_shape=input_shape))
+        model.add(Conv2D(32 * filter_factor, (3, 3), padding='same',
+                         input_shape=input_shape, kernel_initializer='he_normal'))
         model.add(LeakyReLU(alpha=alpha1))
-        model.add(Conv2D(16, (3, 3), padding='same'))
+        model.add(Conv2D(16 * filter_factor, (3, 3), padding='same', kernel_initializer='he_normal'))
         model.add(LeakyReLU(alpha=alpha1))
-        model.add(MaxPooling2D(pool_size=(3, 3), padding='same'))
-        model.add(Dropout(0.1))
+        model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
+        model.add(Dropout(dropout))
         
-        model.add(Conv2D(64, (3, 3), padding='same'))
+        model.add(Conv2D(64 * filter_factor, (3, 3), padding='same', kernel_initializer='he_normal'))
         model.add(LeakyReLU(alpha=alpha1))
-        model.add(Conv2D(16, (3, 3), padding='same'))
+        model.add(Conv2D(16 * filter_factor, (3, 3), padding='same', kernel_initializer='he_normal'))
         model.add(LeakyReLU(alpha=alpha1))
-        model.add(MaxPooling2D(pool_size=(4, 4), padding='same'))
-        model.add(Dropout(0.1)) 
+        model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
+        model.add(Dropout(dropout)) 
         
         model.add(Flatten())
-        model.add(Dense(192))
+        model.add(Dense(1024, kernel_initializer='he_normal'))
         model.add(LeakyReLU(alpha=alpha1))
-        model.add(Dropout(0.1))
-        
-        model.add(Dense(769 * transformation.step))
+        model.add(Dropout(dropout))
+        model.add(BatchNormalization())
+
+        model.add(Dense(769 * transformation.step, kernel_initializer='he_normal'))
         model.add(Reshape((769, transformation.step, 1)))
-        model.add(Activation("sigmoid"))
+        #model.add(ReLU(max_value=0.999, negative_slope=0.01))
+        model.add(Activation('sigmoid'))
         return model
