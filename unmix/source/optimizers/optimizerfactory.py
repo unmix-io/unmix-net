bb9ce710faf98a26b492f67fe605c63b6d06dd5b
Raphael Müller <raaphii@gmail.com>
2019-04-27 22:15:41 +0200

    Added config options to optimizers

--- unmix/source/optimizers/optimizerfactory.py
+++ unmix/source/optimizers/optimizerfactory.py
@@ -15,11 +15,11 @@ class OptimizerFactory(object):
 
     @staticmethod
     def build():
-        optimizer = Configuration.get('training.optimizer')
-        return getattr(OptimizerFactory, optimizer)(OptimizerFactory)
+        optimizer = Configuration.get('training.optimizer.name')
+        return getattr(OptimizerFactory, optimizer)(OptimizerFactory, Configuration.get('training.optimizer'))
 
-    def adam(self, **kwargs):
-        return Adam(**kwargs)
+    def adam(self, options):
+        return Adam(lr=options.lr)
 
-    def rmsprop(self, **kwargs):
-        return RMSprop(**kwargs)
+    def rmsprop(self, options):
+        return RMSprop(options.lr)
