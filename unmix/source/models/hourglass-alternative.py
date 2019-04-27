52732668710a69b8fec592d47fc1520143b63a85
kaufmann-a <kaufmann-a@hotmail.ch>
2019-04-27 14:30:31 +0200

    Fixed shape issues for Hourglass-Alternative

--- unmix/source/models/hourglass-alternative.py
+++ unmix/source/models/hourglass-alternative.py
@@ -37,10 +37,18 @@ class HourglassModel(BaseModel):
 
         outputs = []
         for i in range(config.options.stacks):
-            hourglass = self.__build_hourglass(inter, config.options.stacks)
+            hourglass, padding_added_hor, cropping_added_ver = self.__build_hourglass(inter, config.options.stacks)
+            cropping = ((0, 0), (0, 0))
+            if padding_added_hor and cropping_added_ver:
+                cropping = ((1, 0), (0, 1))
+            elif padding_added_hor and not cropping_added_ver:
+                cropping = ((1, 0), (0, 0))
+            elif not padding_added_hor and cropping_added_ver:
+                cropping = ((0, 0), (0, 1))
 
+            conv = Cropping2D(cropping=cropping)(hourglass)
             conv = Conv2D(128, kernel_size=(3, 3), padding='same',
-                          activation='relu')(hourglass)
+                          activation='relu')(conv)
             conv = Conv2D(128, kernel_size=(1, 1),
                           padding='same', activation='relu')(conv)
 
@@ -57,20 +65,41 @@ class HourglassModel(BaseModel):
     def __build_hourglass(self, input, stacks, size=128):
         upper_1 = Conv2D(size, kernel_size=(3, 3),
                          padding='same', activation='relu')(input)
-
+        padding_added_horizontal = False
+        padding_added_horizontal_upper = False
+        padding_added_vertical = False
+        padding_added_vertical_upper = False
+        if not upper_1.shape[1] % 2 == 0:
+            upper_1 = ZeroPadding2D(padding=((1, 0), (0, 0)))(upper_1)
+            input = ZeroPadding2D(padding=((1, 0), (0, 0)))(input)
+            padding_added_horizontal = True
+        if not upper_1.shape[2] % 2 == 0:
+            upper_1 = ZeroPadding2D(padding=((0, 0), (0, 1)))(upper_1)
+            input = ZeroPadding2D(padding=((0, 0), (0, 1)))(input)
+            padding_added_vertical = True
         # Lower branch
-        pool = MaxPool2D(pool_size=(2, 2), strides=(2, 2))(input)
+        pool = MaxPooling2D(pool_size=(2, 2))(input)
         lower_1 = Conv2D(size, kernel_size=(3, 3),
                          padding='same', activation='relu')(pool)
         # Recursive hourglass
         if stacks > 1:
-            lower_2 = self.__build_hourglass(lower_1, stacks - 1, size)
+            lower_2, padding_added_horizontal_upper, padding_added_vertical_upper = self.__build_hourglass(lower_1, stacks - 1, size)
         else:
             lower_2 = Conv2D(size, kernel_size=(3, 3),
                              padding='same', activation='relu')(pool)
         lower_3 = Conv2D(size, kernel_size=(3, 3),
                          padding='same', activation='relu')(lower_2)
 
-        upper_2 = UpSampling2D(size=tuple(map(lambda x, y: 
-            x.value / y.value, upper_1.shape.dims[1:3], lower_3.shape.dims[1:3])))(lower_3)
-        return Add()([upper_1, upper_2])
+        #upper_2 = UpSampling2D(size=tuple(map(lambda x, y:
+        #    x.value // y.value, upper_1.shape.dims[1:3], lower_3.shape.dims[1:3])))(lower_3)
+        cropping = ((0, 0), (0, 0))
+        if padding_added_horizontal_upper and padding_added_vertical_upper:
+            cropping = ((1, 0), (0, 1))
+        elif padding_added_horizontal_upper and not padding_added_vertical_upper:
+            cropping = ((1, 0), (0, 0))
+        elif not padding_added_horizontal_upper and padding_added_vertical_upper:
+            cropping = ((0, 0), (0, 1))
+
+        upper_2 = Cropping2D(cropping=cropping)(lower_3)
+        upper_2 = UpSampling2D(size=(2, 2))(upper_2)
+        return Add()([upper_1, upper_2]), padding_added_horizontal, padding_added_vertical
